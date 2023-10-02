54fbdce50a1c3ddd813371f8919e3e8243ff807b
duynhat989 <vietduy989kc@gmail.com>
2023-10-02 22:54:02 +0700

    1

---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+// Import router từ file index.js
+const user = require('./routes/user.routes')
+const post = require('./routes/post.routes')
+// Sử dụng router với prefix /api
+const app = express();
+const server = http.createServer(app);
+// ewr
+require('./socket/index')(server)
+// app.use(express.static(path.join(__dirname, 'public')));
+// Phân tích nội dung yêu cầu từ dạng JSON
+app.use(cors());
+// Phân tích nội dung yêu cầu từ dạng x-www-form-urlencoded
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use('/api/user', user);
+app.use('/api/post', post);
+// Khi có một kết nối mới được thiết lập
+const PORT = 1234;
+
+server.listen(PORT, () => console.log(`Server running on port localhost:${PORT}`));
---
+++ utils/db.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mysql = require('mysql');
+var dbConn = mysql.createConnection({
+    host: process.env.HOST_DATABASE,
+    user: process.env.USER_DATABASE,
+    password: process.env.PASS_DATABASE,
+    database: process.env.NAME_DATABASE
+});
+dbConn.connect()
+module.exports = {
+    dbConn
+}
